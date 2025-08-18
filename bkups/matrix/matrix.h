cf4c7cc6a165a42856dfab7508a430f96813d409
leonlza27 <liziao_1114@qq.com>
2025-08-18 16:20:26 +0800

    matrix meta bckup

---
+++ bkups/matrix/matrix.h
@@ -0,0 +1,50 @@
+#ifndef _matrix_
+#define _matrix_
+
+#include <malloc.h>
+#include <cstddef>
+
+#ifdef __ON_DBG
+#include <stdio.h>
+#endif
+
+class matrix{
+private:
+    float* data;
+    size_t rows,cols;//行,列
+public:
+    //初始化:列,行
+    matrix(size_t colsize,size_t rowsize);
+    matrix(const matrix& other);
+
+    inline float& indexVal(size_t row,size_t col);
+    inline float indexVal(size_t row,size_t col) const;
+    inline float& directIndexData(size_t index);
+    inline float directIndexData(size_t index) const;
+
+    void operator=(const float *datainput);
+    void operator=(const matrix& datainput);   
+
+    matrix Transpose() const;
+
+    matrix operator+(const matrix& madd) const;
+
+    matrix operator*(const float mulpnum) const;
+    matrix operator*(const matrix& mulpmtx) const;
+
+    matrix HadamardWith(const matrix& mulpmtx) const; 
+
+
+    ~matrix();
+
+    size_t getRows() const;
+    size_t getCols() const;
+
+    #ifdef __ON_DBG
+
+    void opt(const char* mark = "");
+
+    #endif
+};
+
+#endif
