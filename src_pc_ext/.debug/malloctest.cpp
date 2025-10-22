25e76cf57359a2ee1027d46d27285f4dfde8a0a7
leonlza27 <liziao_1114@qq.com>
2025-10-23 02:01:46 +0800

    SoftMalloc-PC:Construct pooled mem alloc test(1)

--- src_pc_ext/.debug/malloctest.cpp
+++ src_pc_ext/.debug/malloctest.cpp
@@ -4,6 +4,7 @@
 #include <mutex>
 #include <stdlib.h>
 #include <time.h>
+#include <stack>
 
 //模拟元数据分配
 
@@ -25,9 +26,41 @@ public:
     }
 };
 
+//模拟单例对象池
+template<typename T>
+class SimSigInfMemPool{
+private:
+    T *mem;
+    std::stack<T*> ava_space_start;
+public:
+    SimSigInfMemPool(size_t capacity){
+        init(capacity);
+    }
+
+    void init(size_t capacity){
+        mem = (T*)malloc(capacity * sizeof(T));
+        ava_space_start.push(mem);
+    }
+
+    T* palloc(){
+        T* ret = ava_space_start.top();
+        ava_space_start.pop();
+        if(ava_space_start.empty()){
+            ava_space_start.push(ret + 1);
+        }
+        return ret;
+    }
+
+    void pfree(T* ptr){
+        ava_space_start.push(ptr);
+    }
+
+};
+
 template<typename T>
 class PooledSigleAllocator{
 private:
+    SimSigInfMemPool<T> _pool;
 public:
     using value_type = T;
     using pointer = T*;
@@ -42,21 +75,18 @@ public:
         using other = PooledSigleAllocator<U>; 
     }; 
 
-
-    PooledSigleAllocator()noexcept{}
+    PooledSigleAllocator()noexcept : _pool(4097){}
     ~PooledSigleAllocator()noexcept{}
 
     template<typename U>
     PooledSigleAllocator(const PooledSigleAllocator<U>&)noexcept{}
     
     T *allocate(size_t size){
-        printf("Allocate num:%lld\n",size);
-        return static_cast<T*>(::operator new(sizeof(T) * size));
+        return static_cast<T*>(_pool.palloc());
     }
 
     void deallocate(T *ptr, size_t size)noexcept{
-        ::operator delete(ptr);
-
+        _pool.pfree(ptr);
     }    
 
 };
@@ -68,6 +98,7 @@ int main(){
     srand((unsigned int)time(0));
     std::thread testthreads[100];
     
+
     plset.insert({0,0});
 
     return 0;
