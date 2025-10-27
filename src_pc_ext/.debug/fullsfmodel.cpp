5a3e08b9d672930188f9f5984bbdb9d4a77864a4
leonlza27 <liziao_1114@qq.com>
2025-10-28 02:32:24 +0800

    SoftMalloc-PC:Data insert for malloc

--- src_pc_ext/.debug/fullsfmodel.cpp
+++ src_pc_ext/.debug/fullsfmodel.cpp
@@ -3,62 +3,71 @@
 #include <stdint.h>
 
 struct PoolHead{
-    char* ActualData0;
     uint16_t fullsize;
     uint16_t padding;
 };
 
-template<short PByte>
-struct PoolPadding{
-    char _FIX_[PByte];
-};
-
-using ppad4 = PoolPadding<4>;
-using ppad2 = PoolPadding<2>;
-
 //元数据头节点 空闲[end, startNext)
 struct DataNode{
-    uint16_t end;
-    uint16_t startNext;
-    uint16_t prev;
+    int16_t end;
+    int16_t startNext;
+    int16_t prev;
 };
 
 class mempool{
 private:
-    char* mem;
+    char *mem;
+    char *dataStart;
 public:
     mempool(){
         mem = (char*)mmap(0, 8192, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
         PoolHead *head = (PoolHead*)mem;
         head->fullsize = 8192;
         head->padding = 4;
-        head->ActualData0 = mem + sizeof(PoolHead) + (4 - sizeof(PoolHead)%4)*(sizeof(PoolHead)%4 != 0);
-        DataNode *NodeHead = (DataNode*)head->ActualData0;
-        NodeHead->startNext = -1;
-        NodeHead->end = sizeof(DataNode);
-        NodeHead->prev = -1;
+        
+        dataStart = mem + sizeof(PoolHead);
+        ((DataNode*)dataStart)->end = 8;
+        ((DataNode*)dataStart)->prev = -1;
+        ((DataNode*)dataStart)->startNext = -1;
     }
     
     ~mempool(){
+        dataStart = 0;
         munmap(mem, 8192);
     }
 
     void *pAlloc(uint16_t size){
-        uint16_t total_size = size + sizeof(DataNode);
-        uint16_t blocks = total_size/4 + (total_size%4 != 0);
-        ppad4 *idx0 = (ppad4*)((PoolHead*)mem)->ActualData0;
-        ppad4 *cur = idx0;
-        DataNode *curNode;
-        for(curNode = (DataNode*)cur; curNode->startNext != -1;cur = idx0 + curNode->startNext){
-            if(curNode->startNext - curNode->end >= size) break;
-        }
+        size_t total_size = size + sizeof(DataNode);
+        size_t _needed = total_size + (4 - total_size%4)*(total_size%4 != 0);
+        DataNode *curnode;
+        int8_t _idxnxt = 0;
+        int8_t _idxcur = 0;
+        int8_t ava0 = 0;
+        do{
+            curnode = (DataNode*)(dataStart + _idxnxt);
+            ava0 = curnode->end;
+            _idxnxt = curnode->startNext;
+
+            if(_idxnxt - ava0 >= _needed) break;
+
+            _idxcur = _idxnxt;
+
+        }while (_idxnxt != -1);
         
+        //头 4b
+        if(ava0 + _needed > 8188) return 0;
         
-        return 0;
+        DataNode *_insert = (DataNode*)(dataStart + ava0);
+        _insert->startNext = _idxnxt;
+        _insert->end = ava0 + _needed;
+        _insert->prev = _idxcur;
+        
+
+        return dataStart + ava0 + 6;
     }
 
     void pFree(void* ptr){
-        
+        int8_t delidx = ((size_t)ptr) - ((size_t)dataStart) - 6;
     }
 };
 
