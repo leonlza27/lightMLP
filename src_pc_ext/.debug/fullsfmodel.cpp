4c99b70fc1f96fc3acdac9c7bdd40a16b51915f8
leonlza27 <liziao_1114@qq.com>
2025-11-03 01:55:54 +0800

    SoftMalloc-PC:full test 1

--- src_pc_ext/.debug/fullsfmodel.cpp
+++ src_pc_ext/.debug/fullsfmodel.cpp
@@ -2,6 +2,10 @@
 #include <sys/mman.h>
 #include <stdint.h>
 #include <string.h>
+#include <mutex>
+#include <thread>
+#include <stdlib.h>
+#include <time.h>
 
 struct PoolHead{
     uint16_t fullsize;
@@ -19,6 +23,8 @@ class mempool{
 private:
     char *mem;
     char *dataStart;
+
+    std::mutex mallocLock;
 public:
     mempool(){
         mem = (char*)mmap(0, 8192, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
@@ -48,10 +54,9 @@ public:
     }
 
     void *pAlloc(uint16_t size){
+        std::lock_guard<std::mutex> allocG(mallocLock);
         size_t total_size = size + 4;
         size_t _needed = (total_size + 3) & (~3); 
-        
-        printf("input:%d needed:%lu\n",size,_needed);
 
         DataNode *curnode;
         int16_t _idxnxt = 0;
@@ -62,10 +67,8 @@ public:
             _idxnxt = curnode->startNext;
             ava0 = curnode->end;
             if(_idxnxt - ava0 >= _needed) break;
-            printf("idxcur:%d\n",_idxcur);
             _idxcur = _idxnxt;
         }
-        printf("final idxcur:%d ava start:%d\n",_idxcur, ava0);
         if(_idxnxt == -1) return 0;
         
         DataNode *nodeNew = (DataNode*)(dataStart + ava0);
@@ -82,6 +85,7 @@ public:
     }
 
     void pFree(void* ptr){
+        std::lock_guard<std::mutex> freeG(mallocLock);
         if(ptr == 0) return;
         size_t delidx = ((size_t)ptr) - ((size_t)dataStart) - 6;
         if(delidx >= 8172 || delidx < 8) return;
@@ -97,16 +101,36 @@ public:
     }
 };
 
+mempool _mempool;
+
+void testHandler(int _no){
+    size_t _size = rand()%225 + 1;
+    char *testmem = (char*)_mempool.pAlloc(_size);
+    printf("thread %d: try get %lu B...\n", _no, _size);
+    if(testmem == 0) {
+        printf("thread %d: fail\n", _no);
+        return;
+    }
+    int __handleTime = rand()%999 + 1;
+    printf("thread %d: succeed, hold for %d ms\n", _no, __handleTime);
+    std::this_thread::sleep_for(std::chrono::milliseconds(__handleTime));
+    _mempool.pFree(testmem);
+    printf("thread %d: mem returned\n", _no);
+}
+
 int main(){
-    mempool pool;
-    
-    printf("alloc1\n");
-    int *test = (int*)pool.pAlloc(sizeof(int)*5);
-    printf("addr:%lu\n",(size_t)test);
-    printf("\nalloc2\n");
-    int *test2 = (int*)pool.pAlloc(sizeof(int)*5);
-    printf("addr:%lu\n",(size_t)test2);    
-    pool.pFree(test);
+    srand(time(0));
+
+    std::thread testThreads[256];
+    int nu = 0;
+    for(auto &t : testThreads){
+        t = std::thread(testHandler, nu);
+        nu++;
+    }
+
+    for(auto &t : testThreads){
+        t.join();
+    }
     
     return 0;
 }
