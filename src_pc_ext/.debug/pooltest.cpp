fcb6e82d07a317ea4c28556cc5fa405c20a67d36
leonlza27 <liziao_1114@qq.com>
2025-11-12 01:18:37 +0800

    SoftMalloc-PC: build switcher 2

--- src_pc_ext/.debug/pooltest.cpp
+++ src_pc_ext/.debug/pooltest.cpp
@@ -1,32 +1,79 @@
 #include <stdio.h>
-#include <thread>
-#include <vector>
-#include <string>
-#include <sstream>
+#include <sys/mman.h>
 
-#include "spin_lock.h"
+#include "pooleg.h"
 
-struct _wrapeg{
-    std::vector<std::string> globlst;
-    spin_lock lock;
-} wrapeg;
+//4096 total
 
-int main(){
-    std::thread workers[10];
-    for(int i = 0; i < 10 ;i++) workers[i] = std::thread([i](){
-        wrapeg.lock.lock();
-        printf("thread %d working\n", i);
-        for(int j = 0; j < 50; j++){
-            std::stringstream topt;
-            topt << "from thread " << i << " : " << j;
-            wrapeg.globlst.push_back(topt.str());
-        }
-        printf("thread %d quit\n", i);
-        wrapeg.lock.unlock();
-    });
-    for (auto &t : workers) t.join();
+class pllstNode{
+private:
+    alignas(8) uint16_t usableIdx;
+    mempool *pools[510];
+    pllstNode *next; 
+public:
+    pllstNode() : usableIdx(0), next(0){
+        for(auto &i : pools) i = 0;
+    }
 
+    ~pllstNode(){
+        for(auto &i : pools){
+            if(i){
+                i->~mempool();
+                munmap(i, 8192);
+            }
+        } 
+    }
+
+    mempool* GetPool(uint16_t idx) const{
+        return pools[idx];
+    }
+
+    pllstNode *Next() const{
+        return next;
+    }
     
-    for(auto s : wrapeg.globlst) printf("%s\n", s.c_str());
+    //添加池: 成功返回1, 失败则自动添加next&&返回0
+    bool appendPool(){
+        if(usableIdx < 510){            
+            void *pnew = mmap(0, 8192, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+            pools[usableIdx] = new(pnew) mempool;
+            usableIdx++;
+            return 1;
+        }
+        next = (pllstNode*)mmap(0, 4096, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);  
+        next = new(next) pllstNode;
+        return 0;
+    }
+    
+};
+
+class plswitcher{
+private:
+    pllstNode *begin;
+    mempool *hotpools[10]; 
+public:
+    plswitcher(){
+        begin = (pllstNode*)mmap(0, 4096, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+        begin = new(begin) pllstNode;
+        begin->appendPool();
+        for (auto &p : hotpools) {
+            p = 0;
+        }
+        hotpools[0] = begin->GetPool(0);
+    }
+
+    ~plswitcher(){
+        pllstNode *pndefree = begin;
+        while(pndefree){
+            pllstNode *curfree = pndefree;
+            pndefree = curfree->Next();
+            curfree->~pllstNode();
+            munmap(curfree, 4096);
+        }
+    }
+};
+
+int main(){ 
+
 
 }
