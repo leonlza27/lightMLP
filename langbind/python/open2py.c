512819cf448edbf41b2da0107b73d7eb9bbe405c
leonlza27 <liziao_1114@qq.com>
2026-02-05 02:37:08 +0800

    tpdef of netstruct

--- langbind/python/open2py.c
+++ langbind/python/open2py.c
@@ -13,10 +13,37 @@ int netstructpy_init(PyObject *self, PyObject *args, PyObject *args_dict){
     return 0;
 }
 
-void netstructpy_free(PyObject *self){
+void netstructpy_empty(PyObject *self){
     netstruct_py *obj = (netstruct_py*)self;
-    if(obj->netsrc) free(obj->netsrc);
-    
+    if(obj->netsrc) {
+        NetLyrConf *dfree = obj->netsrc;
+        uint32_t size = obj->size;
+        for(uint16_t i = 0; i < size; i++){
+            free(dfree[i].existedBiasData);
+            free(dfree[i].existedWeightData);
+        }
+        free(dfree);
+    }
+    obj->netsrc = 0;
+    obj->size = 0;
+    Py_TYPE(obj)->tp_free(obj);
+}
+
+PyObject *netstructpy_swnet(PyObject *self){
+    netstruct_py *obj = (netstruct_py*)self;
+    NetLyrConf *netprint = obj->netsrc;
+    //format: lyr_no. in out acTp dextra
+    uint32_t size = obj->size;
+    char *opt = (char*)malloc((size + 1) * 52);
+    char curlyr[52];
+    opt[0] = 0;
+    sprintf(opt, "lyr_no.|   in  |  out  | acTp | dextra\n");
+    for(uint32_t i = 0; i < size; i++){
+        NetLyrConf *curp = &netprint[i];
+        sprintf(curlyr, "%6d  %6d  %6d  %6d  %.4f\n", i, curp->existedWeightData->cols, curp->existedWeightData->rows, curp->Activetype, qfix_to_float64(curp->dataExtra));
+        strcat(opt, curlyr);
+    }
+    return PyUnicode_FromString(opt);
 }
 
 PyObject *constructnet(PyObject *self, PyObject *args){
@@ -27,13 +54,18 @@ PyObject *constructnet(PyObject *self, PyObject *args){
         PyErr_SetString(PyExc_ValueError,"incorrect list size: 0 or beyound 65535(max value of unsigned int16)");
         return 0;
     }
+    //网络验证
     for(size_t i = 0; i < size; i++){
         PyObject *lyrtuple_oncheck = PyList_GetItem(lstin, i);
-        int indi, outdi, actp, extra;
+        int indi, outdi, actp, extra, outdilast = 0;
         if(!PyArg_ParseTuple(lyrtuple_oncheck, "iiii", &indi, &outdi, &actp, &extra)) return 0;
         if(indi < 1 || indi > UINT16_MAX){
             PyErr_SetString(PyExc_ValueError,"incorrect in_dim(at tuple[0]) value: lower than 1 or beyound 65535(max value of unsigned int16)");
             return 0;
+        }
+        if(outdilast && indi != outdilast){
+            PyErr_SetString(PyExc_ValueError, "input dim is not equal to the last's output and it is required");
+            return 0;    
         }
         if(outdi < 1 || outdi > UINT16_MAX){
             PyErr_SetString(PyExc_ValueError,"incorrect out_dim(at tuple[1]) value: lower than 1 or beyound 65535(max value of unsigned int16)");
@@ -41,8 +73,34 @@ PyObject *constructnet(PyObject *self, PyObject *args){
         }
     }
 
-
-    netstruct_py *netret = (netstruct_py*)netstructpy_tpdef.tp_alloc(&netstructpy_tpdef, 0);
-
+    //actual allocation
+    netstruct_py *netret = PyObject_New(netstruct_py, &netstructpy_tpdef);
+    netret->size = size;
+    NetLyrConf *netret_data = (NetLyrConf*)malloc(sizeof(NetLyrConf) * size);
+    for(uint16_t i = 0; i < size; i++){
+        PyObject *lyrtuple = PyList_GetItem(lstin, i);
+        int indi, outdi, actp, extra;
+        PyArg_ParseTuple(lyrtuple, "iiii", &indi, &outdi, &actp, &extra);
+        NetLyrConf *_this = &netret_data[i];
+        _this->Activetype = actp;
+        _this->dataExtra = extra;
+        _this->existedBiasData = alloc_matrix_bp(outdi, 1);
+        _this->existedWeightData = alloc_matrix_bp(outdi, indi);
+    }
+    netret->netsrc = netret_data;
     return (PyObject*)netret;
+}
+
+PyMODINIT_FUNC PyInit_core(){
+    PyObject *module;
+    if(PyType_Ready(&netstructpy_tpdef) < 0) return 0;
+    module = PyModule_Create(&lightmlp_topy_root);
+    if(module == 0) return 0;
+    if(PyModule_AddObject(module, "netdef", &netstructpy_tpdef) < 0) goto _ret_err;
+
+    return module;
+
+    _ret_err:
+    Py_DECREF(module);
+    return 0;
 }
--- langbind/python/open2py.h
+++ langbind/python/open2py.h
@@ -1,8 +1,8 @@
 #ifndef _mlpcore2py
 #define _mlpcore2py
 #include <python3.12/Python.h>
-#include "matrixbp2py.h"
 #include "mlpCCore/mlp/mlp.h"
+#include "mlpCCore/matrix/matrix_static.h"
 
 typedef struct _netstruct_py{
     PyObject_HEAD;
@@ -13,19 +13,22 @@ typedef struct _netstruct_py{
 
 static PyObject *netstructpy_new(PyTypeObject *tp, PyObject *args, PyObject *args_dict);
 static int netstructpy_init(PyObject *self, PyObject *args, PyObject *args_dict);
-static void netstructpy_free(PyObject *self);
+static void netstructpy_empty(PyObject *self);
+
+static PyObject *netstructpy_swnet(PyObject *self);
 
 static PyObject *constructnet(PyObject *self, PyObject *args);
 
 static PyTypeObject netstructpy_tpdef = {
     .ob_base = PyVarObject_HEAD_INIT(0, 0)
     .tp_name = "core.netdef",
+    .tp_basicsize = sizeof(netstruct_py),
     .tp_itemsize = 0,
     .tp_flags = Py_TPFLAGS_DEFAULT,
     .tp_new = netstructpy_new,
     .tp_init = netstructpy_init,
-    .tp_free = netstructpy_free,
-    .tp_methods = 0,
+    .tp_dealloc = netstructpy_empty,
+    .tp_repr = netstructpy_swnet,
 };
 
 typedef struct _mlptrain2py{
@@ -40,9 +43,14 @@ typedef struct _mlpexec2py{
     netstruct_py *datasrc_py;   //原始数据,用于引用计数控制
 }mlpexec2py;
 
+static PyMethodDef core_modulefns[] = {
+    {"constructnet", constructnet, METH_VARARGS, 0},
+    {0,0,0,0},
+};
 
 static struct PyModuleDef lightmlp_topy_root = {
-    PyModuleDef_HEAD_INIT, "core", 0, 0
+    PyModuleDef_HEAD_INIT, "core", 0, 0,
+    .m_methods = core_modulefns,
 };
 
 PyMODINIT_FUNC PyInit_core();
