187a8a57b84165c521a55fa296b7a2d1fe97521b
leonlza27 <liziao_1114@qq.com>
2026-03-15 01:00:47 +0800

    matrixbp for java completed

---
+++ langbind/java/mlib/matrixbp.java
@@ -0,0 +1,56 @@
+package mlib;
+
+public class matrixbp{
+    private byte []data;
+
+    static{
+        System.loadLibrary("mbp_tpdef_java");
+    }
+
+    private native void setupmbp(int r, int c);
+    private native String printmatrix();
+
+    public matrixbp(int r, int c){
+        setupmbp(r, c);
+    }
+
+    package mlib;
+
+public class matrixbp{
+    private byte []data;
+
+    static{
+        System.loadLibrary("mbp_tpdef_java");
+    }
+
+    private native void setupmbp(int r, int c);
+    private native String printmatrix();
+
+    public matrixbp(int r, int c){
+        setupmbp(r, c);
+    }
+
+    public native void fromlist(float []lstin) throws IllegalArgumentException;
+    public native void fromrand();
+    
+    public native void toarr_cpy(float []lstin) throws IllegalArgumentException;
+    public native float[] toarr_new();
+    
+    public native int rows();
+    public native int cols();
+
+    @Override
+    public String toString(){
+        return printmatrix();
+    }
+
+}
+    
+    
+
+    @Override
+    public String toString(){
+        return printmatrix();
+    }
+
+}
